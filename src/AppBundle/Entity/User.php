5e45d1041894a2dbc249619433252d51814169a0
Darryl Hein <dhein@xmmedia.com>
2016-11-17 16:37:29 -0700

    removing vich uploader & other use's from user

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -3,17 +3,12 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Doctrine\Common\Collections\ArrayCollection;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\Validator\Constraints as Assert;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use XM\SecurityBundle\Entity\User as BaseUser;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="user")
- * @Vich\Uploadable
  */
 class User extends BaseUser
 {
