c256977e27b2eda5f2b938d2bf332f118ca86fdd
Darryl Hein <dhein@xmmedia.com>
2016-11-30 16:49:32 -0700

    moving maint. listener
    to listener dir since this is where we generally put listeners

--- src/AppBundle/Listener/MaintenanceListener.php
+++ src/AppBundle/Listener/MaintenanceListener.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\EventListener;
+namespace AppBundle\Listener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpFoundation\Response;
