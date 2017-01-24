0f91299f0857616fa606344f7d79cdb617659c03
Darryl Hein <dhein@xmmedia.com>
2017-01-24 10:36:50 -0700

    making admin-delete a global JS component

--- html/js/src/admin.js
+++ html/js/src/admin.js
@@ -3,8 +3,12 @@ import Vue from 'vue';
 import svg_icons from './common/svg_icons';
 import admin_menu_subnav from './admin/admin_menu_subnav';
 import admin_menu_small from './admin/admin_menu_small';
+import admin_delete from './admin/admin_delete';
 import admin_user from './admin/admin_user';
 import list_check from './admin/list_check';
+
+// global components
+Vue.component('admin-delete', admin_delete);
 
 new Vue({
     el: '#app',
--- html/js/src/admin/admin_user.js
+++ html/js/src/admin/admin_user.js
@@ -1,13 +1,8 @@
-import admin_delete from './admin_delete';
-
 export default {
     data: function () {
         return {
             setPassword: false,
         };
-    },
-    components: {
-        'admin-delete': admin_delete,
     },
     watch: {
         'setPassword': function() {
