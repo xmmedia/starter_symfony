ab0fd1d49188b2d5858a893ae3cb15939544d893
Darryl Hein <dhein@xmmedia.com>
2017-07-10 18:21:58 -0500

    removing rebuilding of node-sass
    doesn’t seem like it’s needed any more

--- node_setup.sh
+++ node_setup.sh
@@ -8,8 +8,4 @@ nvm use --delete-prefix --silent
 # install local node packages with yarn
 yarn
 
-# not sure this is needed, but appears yarn doesn't properly install node-sass
-echo "-- Rebuild node-sass"
-npm rebuild node-sass --silent
-
 echo "-- node Setup Done"
