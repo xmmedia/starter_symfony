33a54c7a219c89ecab2861a60045d3c46e5a272c
Darryl Hein <dhein@xmmedia.com>
2018-09-06 16:45:03 -0600

    fix output of local time
    was still displaying UTC

--- html/js/src/common/local_time.vue
+++ html/js/src/common/local_time.vue
@@ -12,14 +12,15 @@ export default {
         locale: {
             type: String,
             default: 'en-ca',
-        }
+        },
     },
 
     computed: {
         displayTime () {
-            const str = new Date(this.datetime).toISOString();
+            const str = new Date(this.datetime)
+                .toLocaleString('en-CA', { hour12: false });
 
-            return str.substring(0, 10)+' '+str.substring(11, 16);
+            return str.substring(0, 10)+' '+str.substring(12, 17);
         },
     },
 }
